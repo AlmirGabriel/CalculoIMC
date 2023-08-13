5542ffc496f18d64fe33acfdacc5c5c82f5809cc
Almir Gabriel <almir.gabriel.andrade@gmail.com>
2023-08-13 01:26:52 -0300

    Formulario pronto

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+function calculo () {
+    onclick(calcularIMC)
+}
+
+document.getElementById('calculo').addEventListener("click", function() {
+    calcularIMC();
+  });
+
+function calcularIMC() {
+    const peso = parseFloat(document.getElementById(peso).value);
+    const altura = parseFloat(document.getElementById(altura).value);
+  
+    const resultado = calcularIMC(peso, altura);
+    const resultadoElement = document.getElementById(resultado);
+    resultadoElement.textContent = `Seu IMC é: ${resultado}`;
+  }
+  
+  function calcularIMC(peso, altura) {
+    if (peso <= 0 || altura <= 0) {
+      return "Peso e altura devem ser valores positivos maiores que zero.";
+    }
+  
+    altura = altura / 100; // Convertendo altura de centímetros para metros
+    const imc = peso / (altura * altura);
+    return imc.toFixed(2);
+  }
+  
